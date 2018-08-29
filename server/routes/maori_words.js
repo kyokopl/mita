3fc160fab158b11a32645ce1440859cb48692a18
Kyoko Pohe <Kyokz@Kyokos-MacBook-Pro.local>
2018-08-30 11:15:04 +1200

    new

---
+++ server/routes/maori_words.js
@@ -0,0 +1,29 @@
+
+const express = require('express')
+const db = require('../db')
+const router = express.Router()
+
+router.get('/', (req, res) => {
+    db.getWords()
+      .then(words => {
+        res.render('index', {words: words})
+      })
+  })
+
+module.exports = router
+
+
+
+// // this is an example of a basic router
+// //copy or rename this file, but dont build all your routes in here
+
+// const router = require('express').Router()
+
+// // router.get('/', (req, res) => {
+// //   dbFile.fbFunction()
+// //     .then(result => {
+// //       res.json(result)
+// //     })
+// // })
+
+// module.exports = router
--- server/server.js
+++ server/server.js
@@ -1,27 +1,15 @@
+const path = require('path')
 const express = require('express')
-const hbs = require('express-handlebars')
-const userRoutes = require('./routes/maori_words')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+
 const server = express()
 
-// Middleware
+server.use(cors('*'))
 
-server.engine('hbs', hbs({extname: 'hbs'}))
-server.set('view engine', 'hbs')
-server.use(express.urlencoded({extended: true}))
+server.use(bodyParser.json())
+server.use(express.static(path.join(__dirname, '../public')))
 
-// Routes
-
-server.use('/', userRoutes)
+//server.use('/api/path', require('./path/to/router'))
 
 module.exports = server
-
-// const path = require('path')
-// const express = require('express')
-// const bodyParser = require('body-parser')
-
-// const server = express()
-
-// server.use(bodyParser.json())
-// server.use(express.static(path.join(__dirname, '../public')))
-
-// module.exports = server
